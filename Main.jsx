2de8d25fe2b4140e2df3763375cd2847db9b45b2
LoveraSantiago <santiago.lovera@gmail.com>
2020-09-26 10:50:39 -0300

    agrup ui tabbones

--- Main.jsx
+++ Main.jsx
@@ -4,13 +4,13 @@
 #include "utils/StringPrototypes.jsx"
 
 #include "factory/FactoryManagerLayers.jsx"
-#include "factory/FactoryBoneManager.jsx"
 #include "factory/FactoryMenuComponents.jsx"
 
-#include "funcionalidades/Opacidade.jsx"
 #include "funcionalidades/ManipulacaoArquivo.jsx"
 #include "funcionalidades/LinhaDoTempo.jsx"
 #include "funcionalidades/SpriteSheet.jsx"
+
+#include "uis/TabBones.jsx"
 
 (function(){
     const doc = app.activeDocument;
@@ -27,28 +27,9 @@
     dlg.alignChildren = "left";    
     var tpanel = dlg.add("tabbedpanel");
     tpanel.preferredSize.width=280;    
-    
-    var tabBones = tpanel.add("tab", undefined, "Bones");
-    var groupEscala = factoryMenuComponents.createGroup(tabBones, "Diminuir escala");    
-    var editTextEscala = groupEscala.add("edittext", undefined, "0");
-    var btnProcessar = groupEscala.add("button", undefined, "Processar");
-    btnProcessar.onClick = function(){      
-        printagem.printar("btn processar pressionado");
-       var manipulacaoArquivo = new ManipulacaoArquivo(app);         
-       var arquivoCopia = manipulacaoArquivo.criarCopia();     
 
-       var managerBones = new FactoryBoneManager();  
-       managerBones.processarLayers(app, doc, arquivoCopia);              
-       manipulacaoArquivo.diminuirEscala(arquivoCopia, parseInt(editTextEscala.text));
-       dlg.close();
-    };
-
-    var groupVisibilidade = factoryMenuComponents.createGroup(tabBones, "Todas Camadas Visiveis");        
-    var btnOpacidade = groupVisibilidade.add("button", undefined, "Processar");
-    btnOpacidade.onClick = function(){
-        var opacidade = new Opacidade();
-        opacidade.todasCamadasVisiveis(doc);
-    };       
+    var tabBones = new TabBones();
+    tabBones.criarTab(app, doc, dlg, tpanel);
     
     var tabSprites = tpanel.add("tab", undefined, "Sprites");
     var groupAcao = factoryMenuComponents.createGroup(tabSprites, "Grupo Acao:");    
---
+++ uis/TabBones.jsx
@@ -0,0 +1,44 @@
+#include "../factory/FactoryBoneManager.jsx"
+#include "../factory/FactoryMenuComponents.jsx"
+
+#include "../funcionalidades/ManipulacaoArquivo.jsx"
+#include "../funcionalidades/Opacidade.jsx"
+
+#include "../utils/Printagem.jsx"
+
+var TabBones = function(){
+
+    const printagem        = new Printagem("TabBones");    
+    const factoryMenuComponents = new FactoryMenuComponents();
+
+    function criarTabBone(app, doc, dlg, painel){
+        var tabBones = painel.add("tab", undefined, "Bones");
+        var groupEscala = factoryMenuComponents.createGroup(tabBones, "Diminuir escala"); 
+        var editTextEscala = groupEscala.add("edittext", undefined, "0");
+        var btnProcessar = groupEscala.add("button", undefined, "Processar");
+
+        btnProcessar.onClick = function(){      
+           printagem.printar("btn processar pressionado");
+           var manipulacaoArquivo = new ManipulacaoArquivo(app);         
+           var arquivoCopia = manipulacaoArquivo.criarCopia();     
+    
+           var managerBones = new FactoryBoneManager();  
+           managerBones.processarLayers(app, doc, arquivoCopia);              
+           manipulacaoArquivo.diminuirEscala(arquivoCopia, parseInt(editTextEscala.text));
+           dlg.close();
+        };
+
+        var groupVisibilidade = factoryMenuComponents.createGroup(tabBones, "Todas Camadas Visiveis");        
+        var btnOpacidade = groupVisibilidade.add("button", undefined, "Processar");
+        btnOpacidade.onClick = function(){
+            var opacidade = new Opacidade();
+            opacidade.todasCamadasVisiveis(doc);
+        };       
+    }
+
+    return{
+        criarTab : function(app, doc, dlg, painel){
+            criarTabBone(app, doc, dlg, painel);
+        }
+    }
+};
